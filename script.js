9075ff3166768f3789ce4915a010976e48419604
Carey LaMothe <carey.lamothe@gmail.com>
2016-02-21 17:44:34 -0800

    add first draft

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+var tiles = [];
+for (var i = 0; i < 63; i++) {
+  tiles[i] = document.createElement('div');
+  tiles[i].style.width = "11.1%";
+  tiles[i].style.paddingBottom = '11.1%';
+  tiles[i].style.float = "left";
+  if ((i % 2) === 0) {
+    tiles[i].style.backgroundColor = 'black';
+   } else {
+     tiles[i].style.backgroundColor = 'red';
+   }
+  body.appendChild(tiles[i]);
+ }
